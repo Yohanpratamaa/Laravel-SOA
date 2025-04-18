7d9a16b82fb8d399feaefa7580bc823d5f4f71d4
yohanpratamaa <yohanpratamaa@gmail.com>
2025-04-18 23:27:04 +0700

    update

--- product-service/app/Http/Controllers/ProductController.php
+++ product-service/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Phpml\Clustering\KMeans;
 
 class ProductController extends Controller
 {
@@ -83,4 +84,37 @@ class ProductController extends Controller
         $product->delete();
         return response()->json(['message' => 'Produk Berhasil Dihapus !']);
     }
+
+    // public function recommend($userId)
+    // {
+    //     // Ambil data pesanan dari OrderService
+    //     $response = Http::get('http://localhost:8002/api/orders/user/' . $userId);
+    //     if (!$response->successful()) {
+    //         return response()->json(['error' => 'No orders found'], 404);
+    //     }
+
+    //     $orders = $response->json();
+    //     $productIds = array_column($orders, 'product_id');
+
+    //     // Ambil semua produk
+    //     $products = Product::all()->toArray();
+    //     $productData = array_map(fn($p) => [$p['price']], $products); // Contoh: Gunakan harga untuk clustering
+
+    //     // Clustering sederhana dengan K-Means
+    //     $kmeans = new KMeans(2);
+    //     $clusters = $kmeans->cluster($productData);
+
+    //     // Temukan produk dalam cluster yang sama dengan produk yang dibeli
+    //     $recommended = [];
+    //     foreach ($productIds as $pid) {
+    //         $product = Product::find($pid);
+    //         if ($product) {
+    //             $index = array_search($product->price, array_column($productData, 0));
+    //             $cluster = array_reduce($clusters, fn($carry, $c) => in_array($index, $c) ? $c : $carry, []);
+    //             $recommended = array_merge($recommended, array_map(fn($i) => $products[$i], $cluster));
+    //         }
+    //     }
+
+    //     return response()->json(array_unique($recommended, SORT_REGULAR));
+    // }
 }
